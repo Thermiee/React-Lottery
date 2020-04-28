821a3d12ae27088385ce43551ff3de3a402227b3
themiee <navyblack16@yahoo.com>
2020-04-28 08:13:59 +0100

    Updates

---
+++ precache-manifest.10410cfbac1c143eb337b0afcb2669ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30c0c5fdb064ca28173943f067f00249",
+    "url": "/React-Lottery/index.html"
+  },
+  {
+    "revision": "eaf7ee0a28ed0918c298",
+    "url": "/React-Lottery/static/css/main.6065f504.chunk.css"
+  },
+  {
+    "revision": "13597667145840357232",
+    "url": "/React-Lottery/static/js/2.b54fb3e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Lottery/static/js/2.b54fb3e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaf7ee0a28ed0918c298",
+    "url": "/React-Lottery/static/js/main.a949da2e.chunk.js"
+  },
+  {
+    "revision": "a464e176cc72faab4ade",
+    "url": "/React-Lottery/static/js/runtime-main.06ac39a3.js"
+  }
+]);
